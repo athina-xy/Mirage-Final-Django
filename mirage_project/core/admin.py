bbafb23196870ff76eb38f024b0f2784ba79584d
ath <A.Xydi@acg.edu>
2025-12-08 00:25:22 +0200

    created admin user + added items

--- mirage_project/core/admin.py
+++ mirage_project/core/admin.py
@@ -1,24 +1,20 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Category, SubCategory, Item
 
-
+# Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('label', 'url_name')
-    prepopulated_fields = {'url_name': ('label',)}
-
+    list_display = ("label", "url_name")
+    prepopulated_fields = {"url_name": ("label",)}
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('label', 'category', 'url_name')
-    list_filter = ('category',)
-    prepopulated_fields = {'url_name': ('label',)}
-
+    list_display = ("label", "category", "url_name")
+    list_filter = ("category",)
+    prepopulated_fields = {"url_name": ("label",)}
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('label', 'category', 'subcategory', 'price', 'rarity')
-    list_filter = ('category', 'subcategory', 'rarity', 'element', 'reality_fragment')
-    search_fields = ('label', 'description', 'element', 'reality_fragment')
+    list_display = ("label", "category", "subcategory", "price", "rarity")
+    list_filter = ("category", "subcategory", "rarity")
+    search_fields = ("label", "description", "element", "reality_fragment")
